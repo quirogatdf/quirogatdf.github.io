78cd2de48cf62dac64bb239834ad7202c8ab0350
quirogatdf <quirogatdf@gmail.com>
2025-03-27 09:27:00 -0300

    feat: update experience and ongoing projects data

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function alerta()
+    {
+    let mensaje;
+    let opcion = confirm("Desea descargar el archivo");
+    if (opcion == true) {
+        window.open("https://drive.google.com/open?id=1SlzkbV43moNFSodJbg2B-UNiFAPsslCr&usp=drive_fs")
+        // document.getElementById("download").href = "https://drive.google.com/file/d/1cXWIWow6y1IJc7Nasah2cwVZCAtA4SUo/view?usp=sharing";
+	};
+}
